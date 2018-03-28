fda62e7eb202a87fce0264a9bf16a94b2b1f09c3
Dmitry Paramonov <dmitry.paramonov@scramteam.com>
2018-03-28 17:03:23 +0300

    dev: wallaby implementation updated

--- wallaby.py
+++ wallaby.py
@@ -4,15 +4,24 @@ from hask import H
 
 
 class TypeConstructor(object):
+    _globals = globals()
+
+    def __init__(self, stateful=False):
+        self.stateful = stateful
+
+    @property
+    def globals(self):
+        return self._globals
+    
+    @globals.setter
+    def globals(self, val):
+        self._globals = val
+
     def __getitem__(self, key):
-        return TypeWrapper(key)
+        return TypeWrapper(key, stateful=self.stateful)
 
     def __getattr__(self, name):
-        return TypeWrapper(eval(name))
-
-
-class StateConstructor(TypeConstructor):
-    pass
+        return TypeWrapper(eval(name, self._globals), stateful=self.stateful)
 
 
 class Chain(object):
@@ -29,20 +38,59 @@ class Chain(object):
 
 
 class TypeWrapper(Chain):
+    def __init__(self, t, stateful=False):
+        super(TypeWrapper, self).__init__(t)
+        self.state = t if stateful else None
+
+    def _action(self, w):
+        if self.state is None:
+            self.state = w.state
+        return super(TypeWrapper, self)._action(w)
+
+    def __getitem__(self, n):
+        return self._chain[n]
+
     def signature(self):
         init_sig = H / self._chain[0]
         return reduce(operator.rshift, self._chain[1:], init_sig)
 
 
 class Pipeline(Chain):
-    def __init__(self, comp, state=None):
-        super(Computation, self).__init__(partial(self._executor, comp, state))
+    def __init__(self, comp, in_type, out_type, comp_name, state=None):
+        super(Pipeline, self).__init__(partial(self._executor, comp, state))
+        self.in_type = in_type
+        self.out_type = out_type
+        self.comp = comp
+        self.comp_name = comp_name
+        self.state = state
+
+    def __call__(self, *args, **kwargs):
+        return self.comp(*args, **kwargs)
 
     def _executor(self, comp, state, ab):
         if state:
             ab.to_stateful(comp, state, comp.__name__)
         else:
-            ab.to(comp, comp.__name__)
+            ab.to(comp)
+
+    def _action(self, p):
+        in_ = p.in_type
+        out = self.out_type
+        if isinstance(out, tuple):
+            out = out[0]
+        if all([out, in_]) and (out != in_ and not issubclass(out, in_)):
+            raise TypeError(
+                "function '{in_func}' expects {in_type} or its subclasses as "
+                "a first argument but {out_type} has been passed from '{out_func}'".format(
+                    in_func=p.comp_name,
+                    in_type=in_,
+                    out_func=self.comp_name,
+                    out_type=out
+                )
+            )
+        self.out_type = p.out_type
+        self.comp_name = p.comp_name
+        return super(Pipeline, self)._action(p)
 
     def init(self, ab):
         """
@@ -54,7 +102,10 @@ class Pipeline(Chain):
 
 class Source(Pipeline):
     def __init__(self, config, pipename):
-        super().__init__(partial(self._executor, config, pipename))
+        super(Pipeline, self).__init__(partial(self._executor, config, pipename))
+        self.in_type = None
+        self.out_type = None
+        self.comp_name = ''
 
     def _executor(self, config, pipename, ab):
         ab.new_pipeline(pipename, config)
@@ -62,8 +113,11 @@ class Source(Pipeline):
 
 class Sink(Pipeline):
     def __init__(self, config):
-        super().__init__(partial(self._executor, config))
-    
+        super(Pipeline, self).__init__(partial(self._executor, config))
+        self.in_type = None
+        self.out_type = None
+        self.comp_name = ''
+
     def _executor(self, config, ab):
         ab.to_sink(config)
 
@@ -76,13 +130,12 @@ def computation(type_sig):
     """
     def _decor(func):
         func = wraps(func)(func ** type_sig.signature())
-        # TODO: get the state from signature
-        state = None
-        return Pipeline(func, state=state)
+        return Pipeline(func, type_sig[0], type_sig[-1], func.__name__, state=type_sig.state)
     return _decor
 
 
 T = TypeConstructor()
-T.State = StateConstructor()
+T.State = TypeConstructor(stateful=True)
+
 
 __all__ = ['T', 'Sink', 'Source', 'computation']
